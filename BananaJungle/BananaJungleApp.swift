bc9ccac27f389b4c6f0a920a63d1a54079988368
keshew <artyom.korotckov2017@yandex.ru>
2025-12-15 13:33:34 +0500

    Initial Commit

---
+++ BananaJungle/BananaJungleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BananaJungleApp.swift
+//  BananaJungle
+//
+//  Created by Артём Коротков on 15.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct BananaJungleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
